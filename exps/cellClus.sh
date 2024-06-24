a0640b8901744d20420806996f3504046084bf7f
epang <jsbyfc@pku.edu.cn>
2024-06-24 15:20:34 +0800

    modify exps/

--- exps/cellClus.sh
+++ exps/cellClus.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 source ~/.bashrc
-module load cuda/11.7.1-cudnn-v8.5.0
-conda activate /work1/superbrain/chenfang2/envs/CGC
+conda activate CGC
 
 [ -z "${batchsize}" ] && batchsize=32
 [ -z "${update_freq}" ] && update_freq=1
@@ -23,24 +22,16 @@ conda activate /work1/superbrain/chenfang2/envs/CGC
 [ -z "${seed}" ] && seed=42
 [ -z "${total_step}" ] && total_step=8000000
 
-
 [ -z "${task}" ] && task=single_cell_task
 [ -z "${loss}" ] && loss=single_cell_loss
 [ -z "${arch}" ] && arch=single_cell_model
 
-[ -z "${user_dir}" ] && user_dir=/work1/superbrain/chenfang2/CGC/src
-[ -z "${unicore_path}" ] && unicore_path=/home4/superbrain/chenfang2/anaconda3/bin/unicore-train
-# [ -z "${data_path}" ] && data_path=/work1/superbrain/chenfang2/lmdb/cellclus_covid
-[ -z "${data_path}" ] && data_path=/work1/superbrain/chenfang2/lmdb/cellclus_PCortex
-
-[ -z "${vocab_path}" ] && vocab_path=/train14/superbrain/zlhu12/lmdb_human_sc_total_new
-[ -z "${text_emb_path}" ] && text_emb_path=/train14/superbrain/chenfang2/train14_zlhu/gene_model_new/src/data/embeding.pickle
-[ -z "${edge_path}" ] && edge_path=/train14/superbrain/zlhu12/data/pert/edge.pickle
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_1_gf/exps/savedir/new_train/pretrain_human_all_target/checkpoint_best.pt
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_1_gf/exps/savedir/new_train/pretrain_human_all_target/checkpoint_3_160000.pt
-
-[ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_new/exps/savedir/pretrain/human_all_gnn_e/checkpoint_30_140000.pt
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_new/exps/savedir/pretrain/human_all_gnn_e/checkpoint_39_180000.pt
+[ -z "${user_dir}" ] && user_dir=../src
+[ -z "${unicore_path}" ] && unicore_path=~/miniconda3/envs/CGC/bin/unicore-train
+[ -z "${data_path}" ] && data_path=../scData/example_datasets/cellclus_PCortex
+[ -z "${vocab_path}" ] && vocab_path=../scData/bioFeature_embs/vocab.json
+[ -z "${text_emb_path}" ] && text_emb_path=../scData/bioFeature_embs/embeding.pickle
+[ -z "${pretrain}" ] && pretrain=../scData/pretrain_weights/CGCompass_pretrain_weights.pt
 
 echo "lr" $lr
 echo "warmup_step" $warmup_step
@@ -60,40 +51,41 @@ echo "savedir" $savedir
 
 # train
 
-# tmp_dir=`mktemp -d`
+tmp_dir=`mktemp -d`
 
-# task_params=""
-# echo "start training"
-# echo source ~/.bashrc 
-# echo "module load cuda/11.7.1-cudnn-v8.5.0"
-# export PYTHONPATH=$user_dir:$PYTHONPATH
-# python ${unicore_path} $data_path  $vocab_path --user-dir $user_dir \
-#        --num-workers $num_worker --ddp-backend=no_c10d \
-#        --task $task --loss $loss --arch $arch \
-#        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 1.0 --allreduce-fp32-grad  \
-#        --lr-scheduler exponential_decay --lr $lr --warmup-updates $warmup_step --decay-ratio $decay_ratio --decay-steps $decay_step --stair-decay \
-#        --batch-size $batchsize --update-freq $update_freq --seed $seed --tensorboard-logdir ${savedir}/tsb/ \
-#        --max-update $total_step --max-epoch $total_epoch --log-interval 1 --log-format simple \
-#        --save-interval-updates $save_interval --validate-interval-updates $validate_interval --keep-interval-updates 40 --no-epoch-checkpoints  \
-#        --save-dir ${savedir} --tmp-save-dir $tmp_dir --required-batch-size-multiple 1 --ema-decay 0.999 --data-buffer-size 512 \
-#        --do_mvg --do_mvc --do_ecs --use_batch_labels --do_dab \
-#        --nlayers 12 --nhead 8 --d_model 512 --d_hid 512 \
-#        --seq_max_len 1201 --mask_ratio 0.4 \
-#        --use_fast_transformer --bf16-sr --bf16 \
-#        --shuffle --ecs_threshold 0.8 --embed --use_gnn --use_graph --text_emb_path=$text_emb_path
+task_params=""
+echo "start training"
+echo source ~/.bashrc 
+echo "module load cuda/11.7.1-cudnn-v8.5.0"
+export PYTHONPATH=$user_dir:$PYTHONPATH
+export CUDA_VISIBLE_DEVICES=0
+python ${unicore_path} $data_path  $vocab_path --user-dir $user_dir \
+       --num-workers $num_worker --ddp-backend=no_c10d \
+       --task $task --loss $loss --arch $arch \
+       --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-8 --clip-norm 1.0 --allreduce-fp32-grad  \
+       --lr-scheduler exponential_decay --lr $lr --warmup-updates $warmup_step --decay-ratio $decay_ratio --decay-steps $decay_step --stair-decay \
+       --batch-size $batchsize --update-freq $update_freq --seed $seed --tensorboard-logdir ${savedir}/tsb/ \
+       --max-update $total_step --max-epoch $total_epoch --log-interval 1 --log-format simple \
+       --save-interval-updates $save_interval --validate-interval-updates $validate_interval --keep-interval-updates 40 --no-epoch-checkpoints  \
+       --save-dir ${savedir} --tmp-save-dir $tmp_dir --required-batch-size-multiple 1 --ema-decay 0.999 --data-buffer-size 512 \
+       --do_mvg --do_mvc --do_ecs --use_batch_labels --do_dab \
+       --nlayers 12 --nhead 8 --d_model 512 --d_hid 512 \
+       --seq_max_len 1201 --mask_ratio 0.4 \
+       --use_fast_transformer --bf16-sr --bf16 \
+       --shuffle --ecs_threshold 0.8 --embed --use_gnn --use_graph --text_emb_path=$text_emb_path --pretrain $pretrain
 
-# rm -rf $tmp_dir
-# echo ""
-# echo "rm -rf $tmp_dir"
+rm -rf $tmp_dir
+echo ""
+echo "rm -rf $tmp_dir"
 
 # test
 
-model_path=/work1/superbrain/chenfang2/downstream_results/cellClus/PCortex/CGC.pt
-savedir=/work1/superbrain/chenfang2/tmp
-export PYTHONPATH=$user_dir:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES="2"
-python ../src/test_clus.py $data_path $model_path $savedir $vocab_path --batch_size $batchsize \
-    --use_batch_labels --do_ecs --do_mvc --do_mvg --do_dab \
-    --seq_max_len 1201  --mask_ratio 0.4 --ecs_threshold 0.8 \
-    --use_fast_transformer --nlayers 12 --nhead 8 --d_model 512 --d_hid 512 \
-    --embed --text_emb_path=$text_emb_path --use_gnn --use_graph
+# model_path=../scData/downstream_weights/cellClus/PCortex/model_CGC.pt
+
+# export PYTHONPATH=$user_dir:$PYTHONPATH
+# export CUDA_VISIBLE_DEVICES="2"
+# python ../src/test_clus.py $data_path $model_path $savedir $vocab_path --batch_size $batchsize \
+#     --use_batch_labels --do_ecs --do_mvc --do_mvg --do_dab \
+#     --seq_max_len 1201  --mask_ratio 0.4 --ecs_threshold 0.8 \
+#     --use_fast_transformer --nlayers 12 --nhead 8 --d_model 512 --d_hid 512 \
+#     --embed --text_emb_path=$text_emb_path --use_gnn --use_graph
--- exps/pert_norman.sh
+++ exps/pert_norman.sh
@@ -1,11 +1,10 @@
 # !/bin/bash
 
 source ~/.bashrc
-module load cuda/11.7.1-cudnn-v8.5.0
-conda activate /work1/superbrain/chenfang2/envs/CGC
+conda activate CGC
 
-[ -z "${batchsize}" ] && batchsize=4
-[ -z "${update_freq}" ] && update_freq=1
+[ -z "${batchsize}" ] && batchsize=16
+[ -z "${update_freq}" ] && update_freq=2
 
 [ -z "${lr}" ] && lr=1e-4 #-6
 [ -z "${warmup_step}" ] && warmup_step=1000
@@ -27,23 +26,13 @@ conda activate /work1/superbrain/chenfang2/envs/CGC
 [ -z "${loss}" ] && loss=pert_loss
 [ -z "${arch}" ] && arch=pert_model
 
-[ -z "${user_dir}" ] && user_dir=/work1/superbrain/chenfang2/CGC/src
-[ -z "${unicore_path}" ] && unicore_path=/home4/superbrain/chenfang2/anaconda3/bin/unicore-train
-
-[ -z "${data_path}" ] && data_path=/train14/superbrain/chenfang2/train14_zlhu/data/pert/pretrain_edge30
-[ -z "${vocab_path}" ] && vocab_path=/train14/superbrain/zlhu12/lmdb_human_sc_total_new/vocab.json
-[ -z "${text_emb_path}" ] && text_emb_path=/train14/superbrain/chenfang2/train14_zlhu/gene_model_new/src/data/embeding.pickle
-[ -z "${edge_path}" ] && edge_path=/train14/superbrain/chenfang2/train14_zlhu/data/pert/edge.pickle
-
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_1_gf/exps/savedir/new_train/pretrain_human_all_target/checkpoint_best.pt
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_1_gf/exps/savedir/new_train/pretrain_human_all_target/checkpoint_3_160000.pt
-[ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_1_gf/exps/savedir/new_train/pretrain_human_all_target/checkpoint_2_120000.pt
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_1_gf/exps/savedir/new_train/pretrain_human_all_target/checkpoint_2_80000.pt
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_1_gf/exps/savedir/new_train/pretrain_human_all_target/checkpoint_1_40000.pt
-
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_new/exps/savedir/pretrain/human_all_gnn_e/checkpoint_30_140000.pt
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_new/exps/savedir/pretrain/human_all_gnn_e/checkpoint_39_180000.pt
-# [ -z "${pretrain}" ] && pretrain=/train14/superbrain/zlhu12/gene_model_new/exps/savedir/pretrain/human_all_gnn_e/checkpoint_50_230000.pt
+[ -z "${user_dir}" ] && user_dir=../src
+[ -z "${unicore_path}" ] && unicore_path=~/miniconda3/envs/CGC/bin/unicore-train
+[ -z "${data_path}" ] && data_path=../scData/example_datasets/Norman
+[ -z "${vocab_path}" ] && vocab_path=../scData/bioFeature_embs/vocab.json
+[ -z "${text_emb_path}" ] && text_emb_path=../scData/bioFeature_embs/embeding.pickle
+[ -z "${edge_path}" ] && edge_path=../scData/example_datasets/Norman/edge.pickle
+[ -z "${pretrain}" ] && pretrain=../scData/pretrain_weights/CGCompass_pretrain_weights.pt
 
 echo "lr" $lr
 echo "batchsize" $batchsize
@@ -64,40 +53,39 @@ echo "savedir" $savedir
 
 # train
 
-# tmp_dir=`mktemp -d`
+tmp_dir=`mktemp -d`
 
-# task_params=""
-# echo "start training"
-# echo source ~/.bashrc 
-# echo "module load cuda/11.7.1-cudnn-v8.5.0"
-# export PYTHONPATH=$user_dir:$PYTHONPATH
-# export CUDA_VISIBLE_DEVICES="0"
-# python ${unicore_path} $data_path $vocab_path --user-dir $user_dir \
-#        --num-workers $num_worker --ddp-backend=no_c10d \
-#        --task $task --loss $loss --arch $arch \
-#        --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-6 --clip-norm 1.0 --allreduce-fp32-grad  \
-#        --lr-scheduler exponential_decay --lr $lr --warmup-updates $warmup_step --decay-ratio $decay_ratio --decay-steps $decay_step --stair-decay \
-#        --batch-size $batchsize --update-freq $update_freq --seed $seed --tensorboard-logdir ${savedir}/tsb/ \
-#        --max-update $total_step --max-epoch $total_epoch --log-interval 1 --log-format simple \
-#        --save-interval-updates $save_interval --validate-interval-updates $validate_interval --keep-interval-updates 40 --no-epoch-checkpoints  \
-#        --save-dir ${savedir} --tmp-save-dir $tmp_dir --required-batch-size-multiple 1 --ema-decay 0.999 --data-buffer-size 128 \
-#        --do_mvg --use_batchnorm --text_emb_path $text_emb_path --edge_path $edge_path \
-#        --use_fast_transformer --shuffle --bf16-sr --bf16  --dropout 0.2 \
-#        --nlayers 12 --nhead 8 --d_model 512 --d_hid 512 
+task_params=""
+echo "start training"
+echo source ~/.bashrc 
+echo "module load cuda/11.7.1-cudnn-v8.5.0"
+export PYTHONPATH=$user_dir:$PYTHONPATH
+export CUDA_VISIBLE_DEVICES="1"
+python ${unicore_path} $data_path $vocab_path --user-dir $user_dir \
+       --num-workers $num_worker --ddp-backend=no_c10d \
+       --task $task --loss $loss --arch $arch \
+       --optimizer adam --adam-betas '(0.9, 0.999)' --adam-eps 1e-6 --clip-norm 1.0 --allreduce-fp32-grad  \
+       --lr-scheduler exponential_decay --lr $lr --warmup-updates $warmup_step --decay-ratio $decay_ratio --decay-steps $decay_step --stair-decay \
+       --batch-size $batchsize --update-freq $update_freq --seed $seed --tensorboard-logdir ${savedir}/tsb/ \
+       --max-update $total_step --max-epoch $total_epoch --log-interval 1 --log-format simple \
+       --save-interval-updates $save_interval --validate-interval-updates $validate_interval --keep-interval-updates 40 --no-epoch-checkpoints  \
+       --save-dir ${savedir} --tmp-save-dir $tmp_dir --required-batch-size-multiple 1 --ema-decay 0.999 --data-buffer-size 128 \
+       --do_mvg --use_batchnorm --text_emb_path $text_emb_path --edge_path $edge_path \
+       --use_fast_transformer --shuffle --bf16-sr --bf16  --dropout 0.2 \
+       --nlayers 12 --nhead 8 --d_model 512 --d_hid 512 
    
 
-# echo ""
-# echo "rm -rf $tmp_dir"
+echo ""
+echo "rm -rf $tmp_dir"
 
 ## test
 
-model_path=/work1/superbrain/chenfang2/downstream_results/scPert/CGC/checkpoint_best.pt
-savedir=/work1/superbrain/chenfang2/tmp
+# model_path=../scData/downstream_weights/scPert/model_CGC.pt
 
-export PYTHONPATH=$user_dir:$PYTHONPATH
-export CUDA_VISIBLE_DEVICES="0"
-python /work1/superbrain/chenfang2/CGC/src/test_pert.py $data_path $vocab_path $model_path $savedir \
-    --batch_size $batchsize --pool_size 300 \
-    --do_mvg --use_batchnorm --dropout 0.0 \
-    --use_fast_transformer --nlayers 12 --nhead 8 --d_model 512 --d_hid 512 \
-    --use_gnn --use_graph --embed --text_emb_path $text_emb_path --edge_path $edge_path
+# export PYTHONPATH=$user_dir:$PYTHONPATH
+# export CUDA_VISIBLE_DEVICES="0"
+# python ../src/test_pert.py $data_path $vocab_path $model_path $savedir \
+#     --batch_size $batchsize --pool_size 300 \
+#     --do_mvg --use_batchnorm --dropout 0.0 \
+#     --use_fast_transformer --nlayers 12 --nhead 8 --d_model 512 --d_hid 512 \
+#     --use_gnn --use_graph --embed --text_emb_path $text_emb_path --edge_path $edge_path
